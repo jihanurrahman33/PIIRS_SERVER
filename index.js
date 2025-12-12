1da4d13345b702729cd8f9868102e35e27645943
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-12-12 17:28:29 +0600

    ping command removed

--- index.js
+++ index.js
@@ -51,10 +51,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
 
     //db and collection info
     const database = client.db("PIIRS");
